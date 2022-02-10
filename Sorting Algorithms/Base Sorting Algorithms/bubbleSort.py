dde41aa5ef0b060fd1477311019e39eb68e79d32
Tazzy <tanlinsir@outlook.com>
2022-02-10 11:36:21 +0000

    Bubble Sort Algorithm Done

--- Sorting Algorithms/Base Sorting Algorithms/bubbleSort.py
+++ Sorting Algorithms/Base Sorting Algorithms/bubbleSort.py
@@ -1,30 +1,40 @@
 
+# WORKING METHOD.
+import random
+def generate_random_list():  # Generates a list of a size between 1 and 20 of integers between 0 and 1000.
+    items = list()
+    for i in range(random.randint(5, 20)):
+        items.append(random.randint(0, 1000))
+    
+    return items
 
 
-items = [41, 15, 17, 32, 18, 28, 77, 54] # 8 items.
+def bubble_sort(items):  # The bubble sort algorithm itself.
+    passes = 0
 
-change_made = True
-
-while change_made:  # Used to optimise the code to stop any more passes from happening if no changes are made.
-    for i in range(len(items)):        
+    for pass_length in range(len(items), 0, -1):  # Each time this loops, a "pass" is made, each time shorter than the one before.
+        index = 0
+        passes += 1
+        current_items = items.copy()
+        while index < pass_length - 1:  
+            if items[index] > items[index + 1]:
+                temp = items[index + 1]
+                items[index + 1] = items[index]
+                items[index] = temp
+            index += 1
         
-        for j in range(len(items) - 1):
-            change_made = False
-            print(items)
-            if items[j] > items[j + 1]:
-                change_made = True
-                print(f"{j} is j")
-                temp = items[j]
-                print(f"{temp} is being moved!")
-                items[j] = items[j + 1]
-                items[j + 1] = temp
-            
-            if not change_made:
-                print("No changes made!")
-                break
-                
-        
-        if not change_made:
+        if current_items == items: # Used for optimacy. If no swaps are made, then the algorithm ends as the array is ordered.
             break
     
+    print("{} passes were made.".format(passes))
+    
+    return items
+
+
+items = generate_random_list()
+print(f"The generated list of items are: {items}")
+
+items = bubble_sort(items)
+
+print(f"\nThe sorted list is: {items}")
 
